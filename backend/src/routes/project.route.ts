44cb67a1a905a7c1c778501200d33cf58076b490
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-04-20 15:33:05 +0530

    started with project services and controllers (create,getall,get) done

---
+++ backend/src/routes/project.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createProjectController, getAllProjectsInWorkspaceController, getProjectByIdAndWorkspaceIdController } from "../controllers/project.controller";
+
+const projectRoutes = Router();
+
+projectRoutes.post("/workspace/:workspaceId/create", createProjectController);
+
+projectRoutes.get("/workspace/:workspaceId/all", getAllProjectsInWorkspaceController);
+
+projectRoutes.get("/:id/workspace/:workspaceId", getProjectByIdAndWorkspaceIdController);
+
+export default projectRoutes;   
---
+++ backend/src/services/project.service.ts
@@ -0,0 +1,73 @@
+import ProjectModel from "../models/project.model";
+import { NotFoundException } from "../utils/appError";
+
+export const createProjectService = async(
+    userId: string, 
+    workspaceId: string,
+    body: {
+        emoji?: string;
+        name: string;
+        description?: string;
+    }
+) => {
+    const project = new ProjectModel({
+        ...(body.emoji && { emoji: body.emoji }),
+        name: body.name,
+        description: body.description,
+        workspace: workspaceId,
+        createdBy: userId,
+    });
+
+    await project.save();
+
+
+    return { project };
+
+} 
+
+export const getProjectsInWorkspaceService = async(
+    workspaceId: string,
+    pageSize: number,
+    pageNumber: number,
+) => {
+    //find all projects in a workspace
+    const totalCount = await ProjectModel.countDocuments({
+        workspace: workspaceId,
+    })
+
+    const skip = (pageNumber - 1) * pageSize;
+
+    const projects = await ProjectModel.find({
+        workspace: workspaceId,
+    })
+    .skip(skip)
+    .limit(pageSize)
+    .populate("createdBy", "_id name profilePicture -password")
+    .sort({ createdAt: -1 })
+
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    return {
+        projects,
+        totalCount, 
+        totalPages,
+        skip,
+    }
+}
+
+export const getProjectByIdAndWorkspaceIdService = async(
+    workspaceId: string,
+    projectId: string,
+) => {
+    const project = await ProjectModel.findOne({
+        _id: projectId,
+        workspace: workspaceId,
+    }).select("_id emoji name description");
+
+    if (!project) {
+        throw new NotFoundException("Project not found");
+    }
+
+    return { project }
+
+}
