528bb45defc68081d28fac5c53b71dbb954694c0
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-04-24 19:16:34 +0530

    Task endpoints done, backend done

--- backend/src/services/task.service.ts
+++ backend/src/services/task.service.ts
@@ -2,7 +2,7 @@ import { TaskPriorityEnum, TaskStatusEnum } from "../enums/task.enum";
 import MemberModel from "../models/member.model";
 import ProjectModel from "../models/project.model";
 import TaskModel from "../models/task.model";
-import { NotFoundException } from "../utils/appError";
+import { BadRequestException, NotFoundException } from "../utils/appError";
 
 export const createTaskService = async (
     workspaceId: string,
@@ -49,4 +49,155 @@ export const createTaskService = async (
     await task.save();
 
     return { task };
+}
+
+export const updateTaskService = async (
+    workspaceId: string,
+    projectId: string,
+    taskId: string,
+    body: {
+        title: string;
+        description?: string;
+        priority: string;
+        status: string;
+        assignedTo?: string | null;
+        dueDate?: string;
+    }
+)=>{
+    const project = await ProjectModel.findById(projectId)
+
+    if(!project || project.workspace.toString() !== workspaceId.toString()){
+        throw new NotFoundException("Project not found in this workspace");
+    }
+
+    const task = await TaskModel.findById(taskId);
+
+    if(!task || task.project.toString() !== projectId.toString()){
+        throw new NotFoundException("Task not found in this project");
+    }
+
+    const updatedTask = await TaskModel.findByIdAndUpdate(
+        taskId,
+        {
+            ...body,
+        },
+        { new : true}
+    );
+    if(!updatedTask){
+        throw new BadRequestException("Failed to update task");
+    }
+
+    return { updatedTask };
+}
+
+
+export const getAllTasksService = async (
+    workspaceId: string,
+    filters: {
+        projectId?: string;
+        status?: string[];
+        priority?: string[];
+        assignedTo?: string[];
+        keyword?: string;
+        dueDate?: string;
+    },
+    pagination: {
+        pageSize: number;
+        pageNumber: number;
+    }
+) => {
+    const query: Record<string, any> = {
+        workspace: workspaceId,
+    }
+
+    if(filters.projectId){
+        query.project = filters.projectId;
+    }
+
+    if(filters.status && filters.status?.length > 0){
+        query.status = {$in: filters.status};
+    }
+
+    if(filters.priority && filters.priority?.length > 0){
+        query.priority = {$in: filters.priority};
+    }
+
+    if(filters.assignedTo && filters.assignedTo?.length > 0){
+        query.assignedTo = {$in: filters.assignedTo};
+    }
+
+    if(filters.keyword && filters.keyword !== undefined){
+        query.title = { $regex: filters.keyword, $options: "i" };
+    }
+
+    if(filters.dueDate) {
+        query.dueDate = {
+            $eq: new Date(filters.dueDate),
+        }
+    }
+
+    const { pageSize, pageNumber} = pagination;
+    const skip = (pageNumber - 1) * pageSize;   
+
+    const [tasks, totalCount] = await Promise.all([
+        TaskModel.find(query).skip(skip)
+            .limit(pageSize)
+            .sort({ createdAt: -1 })
+            .populate("assignedTo", "_id name profilePicture -password")
+            .populate("project", "_id emoji name"),
+        TaskModel.countDocuments(query),
+    ])
+
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    return {
+        tasks,
+        pagination: {
+            pageSize,
+            pageNumber,
+            totalCount,
+            totalPages,
+            skip,
+        }
+    };        
+}
+
+export const getTaskByIdService = async (
+    workspaceId: string,
+    projectId: string,
+    taskId: string
+) => {
+    const project = await ProjectModel.findById(projectId);
+
+    if(!project || project.workspace.toString() !== workspaceId.toString()){
+        throw new NotFoundException("Project not found in this workspace");
+    }
+
+    const task = await TaskModel.findOne({
+        _id: taskId,
+        workspace: workspaceId,
+        project: projectId,
+    }).populate("assignedTo", "_id name profilePicture -password")
+
+    if(!task){
+        throw new NotFoundException("Task not found in this project");
+    }
+    return task ;
+
+}
+
+export const deleteTaskService = async (
+    workspaceId: string,
+    taskId: string
+) => {
+    const task = await TaskModel.findOneAndDelete({
+        _id: taskId,
+        workspace: workspaceId,
+    })
+
+    if(!task){
+        throw new NotFoundException("Task not found in this workspace");
+    }
+
+    return;
 }
